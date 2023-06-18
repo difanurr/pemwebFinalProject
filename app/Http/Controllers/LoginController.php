c5e5d9fcd2ff0efad20887e809facc697d3e1579
difanurr <101717798+difanurr@users.noreply.github.com>
2023-06-18 15:43:05 +0700

    a

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,7 +9,7 @@ class LoginController extends Controller
 {
     public function show()
     {
-        return view('auth.login');
+        return view('pages.login');
     }
 
     public function login(Request $request)
